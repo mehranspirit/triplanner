a6ca10d482ac27fa39281f292d6b3bf090bf4edd
Mehran Hoonejani <mehran.rajaian@gmail.com>
2025-04-15 19:43:14 -0700

    feat: add back button to expenses page for better navigation

--- src/pages/ExpensesPage.tsx
+++ src/pages/ExpensesPage.tsx
@@ -107,6 +107,28 @@ const ExpensesPage: React.FC = () => {
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      {/* Back button */}
+      <div className="mb-6">
+        <Link
+          to={`/trips/${tripId}`}
+          className="inline-flex items-center text-sm text-gray-600 hover:text-gray-900"
+        >
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            className="h-5 w-5 mr-1" 
+            viewBox="0 0 20 20" 
+            fill="currentColor"
+          >
+            <path 
+              fillRule="evenodd" 
+              d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" 
+              clipRule="evenodd" 
+            />
+          </svg>
+          Back to Trip Details
+        </Link>
+      </div>
+
       <div className="bg-white shadow rounded-lg overflow-hidden">
         <Link to={`/trips/${tripId}`} className="block">
           <div className="relative h-48 group">
